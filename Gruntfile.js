f5d3aef4f40741e246a069e00dc82f2cabbfe573
Arnau March <arnau@arnaumarch.com>
2015-02-27 12:04:03 +0100

    updated styles

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function(grunt) {
     less: {
       development: {
         files: {
-          "css/buttons.css": "less/buttons.less",
+          "css/buttons.telefonica.css": "less/buttons.less",
           "css/buttons.movistar.css": "less/movistar.less",
           "css/buttons.o2.css": "less/o2.less",
           "css/buttons.vivo.css": "less/vivo.less"
@@ -18,7 +18,7 @@ module.exports = function(grunt) {
           optimization: 2
         },
         files: {
-          "css/buttons.min.css": "less/buttons.less",
+          "css/buttons.telefonica.min.css": "less/buttons.less",
           "css/buttons.movistar.min.css": "less/movistar.less",
           "css/buttons.o2.min.css": "less/o2.less",
           "css/buttons.vivo.min.css": "less/vivo.less"
