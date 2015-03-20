747f858c6f199c294b2d708f5aa4b1b9f83d9891
Arnau March <arnau@arnaumarch.com>
2015-03-20 11:04:18 +0100

    improved grunt workflow

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,12 +25,36 @@ module.exports = function(grunt) {
           'git push origin gh-pages',
           'git checkout master'
         ].join('&&')
+      },
+
+      // adds prompted commit message
+      message: {
+        command: function() {
+          var message = grunt.config('gitmessage');
+          return "git commit -am '" + message + "'";
+        }
+      }
+    },
+
+    prompt: {
+      commit: {
+        options: {
+          questions: [
+            {
+              config: 'gitmessage',
+              type: 'input',
+              message: 'Commit Message'
+            }
+          ]
+        }
       }
     }
   });
 
   grunt.registerTask('release', [
     'exec:add',
+    'prompt',
+    'exec:message',
     'bump',
     'exec:pages'
   ]);
